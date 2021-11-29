67a563b88213b46164307dff0268582946894684
Vlad Anisov <v.anisov@icode.by>
2021-11-29 14:29:51 +0300

    fix

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def start_message(message):
-    try:
-        cities = message.text.split(",")
-        cities = [city.strip() for city in cities]
-        first_part(cities, bot, message)
-        get_map(cities, cities, bot, message)
-    except Exception as e:
-        bot.send_message(message.chat.id, f'Что-то пошло не так: {e}')
+    # try:
+    cities = message.text.split(",")
+    cities = [city.strip() for city in cities]
+    get_map(cities, cities, bot, message)
+    first_part(cities, bot, message)
+    # except Exception as e:
+    #     bot.send_message(message.chat.id, f'Что-то пошло не так: {e}')
 
 
 if __name__ == '__main__':
