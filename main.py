20eda9d722d2e1038fbe46fe4fbf4841f63ff93d
Vlad Anisov <v.anisov@icode.by>
2021-11-29 09:54:57 +0300

    fix

--- main.py
+++ main.py
@@ -10,28 +10,31 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(content_types=['text'])
 def start_message(message):
-    cities = message.text.split(",")
-    cities = [city.strip() for city in cities]
-    first_part(cities)
-    with open('first_table.xlsx', 'rb') as file:
-        bot.send_document(message.chat.id, file)
-    get_map(cities, cities)
-    # with open('Матрица расстояний.txt', encoding='utf8') as text:
-    #     bot.send_message(message.chat.id, text.read())
-    with open('matrix.png', 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
-    with open('Расположение городов на карте.png', 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
-    with open('Соединение городов методом ветвей и границ.png', 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
-    with open('Соединение городов методом ветвей и границ.txt', encoding='utf8') as text:
-        bot.send_message(message.chat.id, text.read())
-    with open('Соединение городов c поперечной связью.png', 'rb') as photo:
-        bot.send_photo(message.chat.id, photo)
-    with open('Соединение городов c поперечной связью.txt', encoding='utf8') as text:
-        bot.send_message(message.chat.id, text.read())
-    with open('Пути.txt', encoding='utf8') as text:
-        bot.send_message(message.chat.id, text.read())
+    try:
+        cities = message.text.split(",")
+        cities = [city.strip() for city in cities]
+        first_part(cities)
+        with open('first_table.xlsx', 'rb') as file:
+            bot.send_document(message.chat.id, file)
+        get_map(cities, cities)
+        # with open('Матрица расстояний.txt', encoding='utf8') as text:
+        #     bot.send_message(message.chat.id, text.read())
+        with open('matrix.png', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
+        with open('Расположение городов на карте.png', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
+        with open('Соединение городов методом ветвей и границ.png', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
+        with open('Соединение городов методом ветвей и границ.txt', encoding='utf8') as text:
+            bot.send_message(message.chat.id, text.read())
+        with open('Соединение городов c поперечной связью.png', 'rb') as photo:
+            bot.send_photo(message.chat.id, photo)
+        with open('Соединение городов c поперечной связью.txt', encoding='utf8') as text:
+            bot.send_message(message.chat.id, text.read())
+        with open('Пути.txt', encoding='utf8') as text:
+            bot.send_message(message.chat.id, text.read())
+    except Exception as e:
+        bot.send_message(message.chat.id, f'Что-то пошло не так {e}')
 
 
 if __name__ == '__main__':
